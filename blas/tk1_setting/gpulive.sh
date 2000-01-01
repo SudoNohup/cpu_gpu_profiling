9ed7e7a1f3ac07bbbbe233580ce3b0635b66340f
Jianyu Huang <jianyu@cs.utexas.edu>
2000-01-01 22:06:24 +0000

    update gpu tk1 setting

--- blas/tk1_setting/gpulive.sh
+++ blas/tk1_setting/gpulive.sh
@@ -4,3 +4,8 @@ cat /sys/kernel/debug/clock/override.gbus/rate
 cat /sys/kernel/debug/clock/override.gbus/state
 cat /sys/kernel/debug/clock/gbus/rate
 
+echo 924000000 > /sys/kernel/debug/clock/override.emc/rate
+echo 1 > /sys/kernel/debug/clock/override.emc/state
+cat /sys/kernel/debug/clock/override.emc/rate
+cat /sys/kernel/debug/clock/override.emc/state
+cat /sys/kernel/debug/clock/emc/rate
